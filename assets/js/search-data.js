d834ecede4d6fd45ce5b3fed42d8370a1807bbbe
raja-marjieh <64031078+raja-marjieh@users.noreply.github.com>
2025-02-05 16:48:47 +0000

    Deploying to gh-pages from @ raja-marjieh/raja-marjieh.github.io@19d21de7ba8531e6e3e5fa2b42bda3f1baaf6e07 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -389,16 +389,16 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+    },{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
+              window.location.href = "/news/prev_announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-this-is-my-new-website-more-updates-on-the-way",
+          title: 'This is my new website, more updates on the way!',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
