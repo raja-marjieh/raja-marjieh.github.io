39655ff9ace1b64616f4dba62e717833c4b3ea75
raja-marjieh <64031078+raja-marjieh@users.noreply.github.com>
2025-02-05 23:18:43 +0000

    Deploying to gh-pages from @ raja-marjieh/raja-marjieh.github.io@280a21d08d5518a1c55c594446ca34969083f32d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -397,9 +397,9 @@ ninja.data = [{
           description: "Aspects of Similarity in Natural and Artificial Minds",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-representations-in-music",
+          title: 'Representations in Music',
+          description: "Ongoing Work on Musical Representations and Beyond",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
