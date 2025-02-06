af7ff3f29da41e2f96f5bdc18861a0de0dc59cd3
raja-marjieh <64031078+raja-marjieh@users.noreply.github.com>
2025-02-06 04:50:26 +0000

    Deploying to gh-pages from @ raja-marjieh/raja-marjieh.github.io@814d12269803155752c220bb2e35f7f5c60e0adf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -412,9 +412,9 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-quantum-optics",
+          title: 'Quantum Optics',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",
