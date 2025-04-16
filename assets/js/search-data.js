a901d90674916b2814ac2611ee0262710b251f2f
raja-marjieh <64031078+raja-marjieh@users.noreply.github.com>
2025-04-16 03:15:11 +0000

    Deploying to gh-pages from @ raja-marjieh/raja-marjieh.github.io@04cdd39bb378124b93f08b851b18345ac925a93f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "You can download my CV through the PDF button on the top right (last updated - February 6th, 2025).",
+          description: "You can download my CV through the PDF button on the top right (last updated - April 16th, 2025).",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
